1f1458bc5d17e7addf4fe8cd54276d9027d68218
Administrator <Administrator@windows10.microdone.cn>
2019-07-05 23:04:27 +0800

    首次提交

---
+++ src/main/java/com/lx/service/BookService.java
@@ -0,0 +1,32 @@
+package com.lx.service;
+
+import java.util.List;
+
+import com.lx.dto.AppointExecution;
+import com.lx.entity.Book;
+
+public interface BookService {
+	/**
+	 * 查询一本图书
+	 * 
+	 * @param bookId
+	 * @return
+	 */
+	Book getById(long bookId);
+
+	/**
+	 * 查询所有图书
+	 * 
+	 * @return
+	 */
+	List<Book> getList();
+
+	/**
+	 * 预约图书
+	 * 
+	 * @param bookId
+	 * @param studentId
+	 * @return
+	 */
+	AppointExecution appoint(long bookId, long studentId);
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<a href="book/1000/appoint?studentId=12345678910">
+		嗯嗯
+	</a>
+</body>
+</html>
